d86b608a686ae83620a67ef0608d2cc8a437c3ed
Ammar Ahmed <ammarahmed12805@gmail.com>
2024-12-15 02:40:28 +0500

    upload 7 files

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const divRoot = document.getElementById('root');
+
+ReactDOM.createRoot(divRoot).render(
+  <div>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quia ad provident consequuntur hic, sapiente eligendi sunt amet id consectetur ratione odio asperiores? Labore, libero totam temporibus asperiores cum quod exercitationem recusandae sed ut expedita dignissimos omnis nisi id adipisci aut, officia alias beatae et laudantium architecto vitae amet pariatur! Maiores similique eius odit est, ab eos magnam harum odio modi, repellat debitis autem reiciendis aliquid velit saepe. Ipsam corporis ut saepe architecto, suscipit earum doloremque eligendi accusantium neque dolorem natus. Praesentium, non? Quo quam consequatur labore, nisi rerum exercitationem, aut eveniet placeat, incidunt aperiam pariatur nam architecto illum soluta?</p>
+    <img src="/public/vite.svg" alt="" />
+  </div>
+)
